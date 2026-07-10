ebf092d656e75e84e450ade7c5d5e1e36b4a135e
sebasemr <u20221b850@upc.edu.pe>
2026-07-10 14:50:09 -0500

    IP

--- src/main/java/com/upc/purevibeb/security/config/SecurityConfig.java
+++ src/main/java/com/upc/purevibeb/security/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
         // Permite peticiones SÓLO desde tu frontend de Angular
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:4200",
-                "http://54.92.130.37:4200"
+                "http://32.197.237.13:4200"
         ));
         // Permite los métodos HTTP que usaremos
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
